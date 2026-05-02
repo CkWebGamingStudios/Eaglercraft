4f3a8202fa5a65b66ac2bfd5cc0e2e6d93082ced
Ck Web Gaming Studios <ckwebgaming@gmail.com>
2026-05-02 19:37:54 +0530

    Split launcher into multiple working HTML pages

---
+++ assets/data.js
@@ -0,0 +1,28 @@
+window.LAUNCHER_DATA = {
+  web: [
+    {name:"Eaglercraft 1.12.2 (WASM)", version:"1.12.2", path:"Versions/Eaglercraft_1.12.2_WASM.html"},
+    {name:"EaglercraftX 1.8.8 (WASM)", version:"1.8.8", path:"Versions/EaglercraftX_1.8.8_WASM.html"},
+    {name:"Eaglercraft 1.6.4", version:"1.6.4", path:"Versions/Eaglercraft_1.6.4.html"}
+  ],
+  modded: [
+    {name:"Shadow v4.5", version:"latest", path:"Clients/Shadow/Shadow.v4.5.optifine.html"},
+    {name:"Resent v4.0.1", version:"latest", path:"Clients/Resent/Resent v4.0.1.html"},
+    {name:"Astra v2", version:"latest", path:"Clients/Astra/astra v2 1.8.html"},
+    {name:"Nova", version:"latest", path:"Clients/Nova/Nova.html"},
+    {name:"Wispcraft v3", version:"latest", path:"Clients/Wispcraft/Wispcraft v3.html"}
+  ],
+  mobile: [
+    {name:"Shadow Mobile", version:"v2.5", path:"Clients/Shadow/Shadow.v2.5.html"},
+    {name:"Resent Mobile", version:"v4.0.1", path:"Clients/Resent/Resent v4.0.1.html"}
+  ],
+  skins: [
+    {name:"Cultist", path:"Tools/Skins/cultist.png"},
+    {name:"Dream Mask", path:"Tools/Skins/dreamMask.png"}
+  ],
+  tools: [
+    {name:"Mods Folder", path:"Mods/"},
+    {name:"Versions Folder", path:"Versions/"},
+    {name:"Clients Folder", path:"Clients/"},
+    {name:"Skins Folder", path:"Tools/Skins/"}
+  ]
+};
